f0def29ae0ed5962cdb49345b2a84fe434f1e988
mbrembati <marco@brembati.it>
2023-12-02 17:42:26 +0100

    Added example

---
+++ examples/Arduino_Uno_R4WIFI_3DigitDisplay.ino
@@ -0,0 +1,32 @@
+#include <ArduinoR43DigitDisplay.h>
+
+#include "Arduino_LED_Matrix.h"
+
+ArduinoLEDMatrix matrix;
+
+void setup() {
+  Serial.begin(115200);
+  matrix.begin();
+}
+
+uint8_t frame[8][12] = {
+  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+};
+
+void loop(){
+   // integer or fp argument - automatic format
+    drawNumber(49.1,frame);
+    matrix.renderBitmap(frame, 8, 12);
+    delay(5000);
+   // integer or fp argument - fixed decimals format
+    drawNumber(5.123,frame,2);
+    matrix.renderBitmap(frame, 8, 12);
+    delay(5000);
+}
